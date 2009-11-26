bc1bc83799c2e1b3553598499b20d68985bc51d5
Chris <chris@chrisblunt.com>
2009-11-26 13:14:37 +0000

    Prepared for RSpec and Cucumber

--- config/environment.rb
+++ config/environment.rb
@@ -19,6 +19,10 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem "rspec", :lib => false
+  config.gem "rspec-rails", :lib => false
+  config.gem "cucumber"
+  config.gem "webrat"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
