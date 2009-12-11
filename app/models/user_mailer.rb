022df2a91fb52990cef7b0a0ab00b920a44b4e19
Chris Blunt <chris@chrisblunt.com>
2009-12-11 07:57:22 +0000

    Changed notification mailer to use local hostname for sender email address

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ActionMailer::Base
 
   def verification_email(user)
-    from            "Notifications <notifications@chrisblunt.com>"
+    from            "Molehill Notifications <notifications@#{ActionMailer::Base.default_url_options[:host]}>"
     recipients      "#{user.email_address}"
     subject         "Please verify your account"
     body            :user => user
